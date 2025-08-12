e4aab17e566cebff24e90b223c73d388b924fc4a
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-13 00:31:55 +0700

    #2 - response(api-response): Init ApiResponse global

--- src/main/java/go_phone/common/response/ApiResponse.java
+++ src/main/java/go_phone/common/response/ApiResponse.java
@@ -1,4 +1,36 @@
 package go_phone.common.response;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String code;
+
+    private T data;
+
+    private LocalDateTime timestamp;
+
+    public ApiResponse(boolean success, String message, String code, T data) {
+        this.success = success;
+        this.message = message;
+        this.code = code;
+        this.data = data;
+        this.timestamp = LocalDateTime.now();
+    }
 }
