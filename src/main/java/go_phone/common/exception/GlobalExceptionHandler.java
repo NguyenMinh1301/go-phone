bda0c761dc886e19e54bf20de70416920612e599
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-13 00:31:00 +0700

    #2 - exception(handler-exception): Init base handle global exception

--- src/main/java/go_phone/common/exception/GlobalExceptionHandler.java
+++ src/main/java/go_phone/common/exception/GlobalExceptionHandler.java
@@ -1,4 +1,26 @@
 package go_phone.common.exception;
 
+import go_phone.common.response.ApiResponse;
+import go_phone.common.response.ResponseHandler;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.sql.SQLSyntaxErrorException;
+
 public class GlobalExceptionHandler {
+    @ExceptionHandler(AppException.class)
+    public ResponseEntity<ApiResponse<Object>> handleAppException(AppException e) {
+        return ResponseHandler.error(e.getErrorCode());
+    }
+
+    @ExceptionHandler(SQLSyntaxErrorException.class)
+    public ResponseEntity<ApiResponse<Object>> handleSQLException(SQLSyntaxErrorException ex) {
+        return ResponseHandler.error(ErrorCode.BAD_SQL, HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Object>> handleOther(Exception ex) {
+        return ResponseHandler.error(ErrorCode.INTERNAL_ERROR, HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage());
+    }
 }
