33970a181ce6ec7e063f633d12570904dad30a81
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-13 18:41:18 +0700

    config(offset): Calculate offset method

---
+++ src/main/java/go_phone/common/handler/CalculateOffset.java
@@ -0,0 +1,14 @@
+package go_phone.common.handler;
+
+public class CalculateOffset {
+
+    // Tính offset cho Pageable
+    public int calculateOffset(int page, int size) {
+
+        // int offset = (page - 1) * size;
+
+        return (page - 1) * size;
+
+    }
+
+}
