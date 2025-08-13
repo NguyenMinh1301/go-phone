514112001f7c8e7214b55cc34b2cd77855f2c4a3
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-13 13:30:58 +0700

    chore(Api-url/Error-code): update global url/error-code

--- src/main/java/go_phone/common/constants/ApiConstants.java
+++ src/main/java/go_phone/common/constants/ApiConstants.java
@@ -26,9 +26,23 @@ public class ApiConstants {
         public static final String ADD = "/create";
         public static final String UPDATE = "/update/{id}";
         public static final String SOFT_DELETE = "/delete/{id}";
-        public static final String DELETE = "/delete/{id}";
 
         private Product() {
+        }
+    }
+
+    public static final class Category {
+        public static final String BASE = VERSION_V1 + "/category";
+        public static final String BRAND = "/brand";
+        public static final String COLOR = "/color";
+        public static final String MADE_FROM = "/made-from";
+        public static final String GET_ALL = "/get";
+        public static final String GET_BY_ID = "/get/{id}";
+        public static final String ADD = "/create";
+        public static final String UPDATE = "/update/{id}";
+        public static final String SOFT_DELETE = "/delete/{id}";
+
+        private Category() {
         }
     }
 
--- src/main/java/go_phone/common/exception/ErrorCode.java
+++ src/main/java/go_phone/common/exception/ErrorCode.java
@@ -2,10 +2,27 @@ package go_phone.common.exception;
 
 import lombok.Getter;
 
+/**
+ * Mã lỗi hệ thống chia theo nhóm:
+ * - 5000: Lỗi hệ thống / bảo mật / xác thực
+ * - 4000: Lỗi từ dịch vụ bên ngoài (Google, Vonage,...)
+ * - 3000: Lỗi định dạng / dữ liệu không hợp lệ
+ * - 2000: Không tìm thấy / đã tồn tại
+ */
+
 @Getter
 public enum ErrorCode {
 
+    //=========== PRODUCT ===========
     PRODUCT_NOT_FOUND("2000", "Không tìm thấy sản phẩm"),
+    PRODUCT_EXISTED("2001", "Sản phẩm đã tồn tại"),
+    PRODUCT_EMPTY("2002", "Không có sản phẩm tồn tại"),
+
+    BRAND_NOT_FOUND("2100", "Thuộc tính đã tồn tại"),
+    BRAND_EXISTED("2101", "Không tìm thấy thuộc tính"),
+    BRAND_EMPTY("2102", "Không có thuộc tính tồn tại"),
+
+
     BAD_SQL("1001", "Sai câu lệnh sql"),
     INTERNAL_ERROR("5000", "Lỗi hệ thống"),
     ;
