717970fec9c7aed238b59edfc4afa71b0da28062
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-19 19:18:37 +0700

    remove(dotenv): remove lib load .env

--- src/main/java/go_phone/Application.java
+++ src/main/java/go_phone/Application.java
@@ -1,6 +1,5 @@
 package go_phone;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,28 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-
-		// Load .env
-		loadEnv();
-
 		SpringApplication.run(Application.class, args);
-	}
-
-	private static void loadEnv() {
-		Dotenv dotenv = Dotenv.configure()
-				.directory("./")
-				.filename(".env")
-				.load();
-
-		// Database
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
-		// JWT
-		System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
-		System.setProperty("JWT_ISSUER", dotenv.get("JWT_ISSUER"));
-		System.setProperty("JWT_ACCESS_EXP_MINUTES", dotenv.get("JWT_ACCESS_EXP_MINUTES"));
 	}
 
 }
