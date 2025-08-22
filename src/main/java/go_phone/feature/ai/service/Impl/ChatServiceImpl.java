ac411b8f757d8b22a81676139e16e174f5a2fa3f
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-22 13:15:38 +0700

    update(prompt): add system prompt

--- src/main/java/go_phone/feature/ai/service/Impl/ChatServiceImpl.java
+++ src/main/java/go_phone/feature/ai/service/Impl/ChatServiceImpl.java
@@ -3,6 +3,9 @@ package go_phone.feature.ai.service.Impl;
 import go_phone.feature.ai.dto.ChatRequest;
 import go_phone.feature.ai.service.ChatService;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,8 +19,17 @@ public class ChatServiceImpl implements ChatService {
 
     @Override
     public String chat(ChatRequest request) {
+
+        SystemMessage systemMessage = new SystemMessage("""
+                Your name is gophone
+                """);
+
+        UserMessage userMessage = new UserMessage(request.message());
+
+        Prompt promt = new Prompt(systemMessage, userMessage);
+
         return chatClient
-                .prompt(request.message())
+                .prompt(promt)
                 .call()
                 .content();
     }
