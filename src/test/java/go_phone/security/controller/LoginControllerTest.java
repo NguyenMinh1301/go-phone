d0fbdb140012e192ea0a5d933e8de8d32a9b0c53
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-25 13:13:59 +0700

    init(test): init spring-test for /login

---
+++ src/test/java/go_phone/security/controller/LoginControllerTest.java
@@ -0,0 +1,75 @@
+package go_phone.security.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import go_phone.security.configuration.AuthService;
+import go_phone.security.dto.request.LoginRequest;
+import go_phone.security.dto.response.TokenResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.time.Instant;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Mock
+    private AuthService authService;
+
+    private LoginRequest loginRequest;
+    private TokenResponse tokenResponse;
+
+    Instant now = Instant.now();
+    Instant exp = now.plusSeconds(120 * 60);
+
+    @BeforeEach
+    void initData () {
+        loginRequest = LoginRequest.builder()
+                .username("minh2")
+                .password("123456")
+                .build();
+
+        tokenResponse = TokenResponse.builder()
+                .issuedAt(now)
+                .expiresAt(exp)
+                .issuer("go_phone")
+                .username("minh2")
+                .build();
+
+    }
+
+    @Test
+    void login_validRequest_success() throws Exception {
+        //GIVEN
+        ObjectMapper objectMapper = new ObjectMapper();
+        String content = objectMapper.writeValueAsString(loginRequest);
+
+        Mockito.when(authService.login(ArgumentMatchers.any())).thenReturn(tokenResponse);
+
+        //WHEN, THEN
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/auth/login")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("success").value(true)
+        );
+
+    }
+
+}
