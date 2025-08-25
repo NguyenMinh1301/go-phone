449e0a344eea5febcf6e3a7f81d9f89598b34db6
Nguyen Quang Minh <nguyenminh1301.dev@gmail.com>
2025-08-25 13:13:18 +0700

    update(login-request): add @Builder

--- src/main/java/go_phone/security/dto/request/LoginRequest.java
+++ src/main/java/go_phone/security/dto/request/LoginRequest.java
@@ -2,12 +2,14 @@ package go_phone.security.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class LoginRequest {
 
